78207c25a34301099bc65c205cb2ee005eb9b577
KemalIlkilic <kemalonur.ilkilic@bahcesehir.edu.tr>
2024-01-12 16:37:15 +0300

    Add Pomodoro Timer notifications and update timer display

--- background.js
+++ background.js
@@ -6,10 +6,20 @@ chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "pomodoroTimer") {
     chrome.storage.local.get(["timer", "isRunning"], (res) => {
       if (res.isRunning) {
+        let timer = res.timer + 1;
+        let isRunning = true;
+        if (timer === 60 * 25) {
+          this.registration.showNotification("Pomodoro Timer", {
+            body: "25 minutes has passed!",
+            icon: "icon.png",
+          });
+          timer = 0;
+          isRunning = false;
+        }
         chrome.storage.local.set({
-          timer: res.timer + 1,
+          timer,
+          isRunning,
         });
-        console.log(res.timer);
       }
     });
   }
